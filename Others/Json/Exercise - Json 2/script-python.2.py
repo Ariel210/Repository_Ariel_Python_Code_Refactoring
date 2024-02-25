916522a292915a34587de448aa4b208ce3e741e7
Ariel Izurieta <ariel_izurieta@hotmail.com>
2024-02-25 12:51:21 -0300

    update on dumps methos in json and python by ariel

--- Others/Json/Exercise - Json 2/script-python.2.py
+++ Others/Json/Exercise - Json 2/script-python.2.py
@@ -3,9 +3,11 @@ import json
 def second_exercise_json():
     with open('Others/Json/Exercise - Json 2/script-2.json') as json_file_2:
         load_dates = json.load(json_file_2)
+        dumps_dates = json.dumps(load_dates)
         
         print(load_dates)
         print(type(load_dates))
+        print(f"Dumps Method: {dumps_dates}")
         
         print("\n")
         
