58d9a567742a9eea886cb48d29ae8b48dec3c219
Ariel Izurieta <ariel_izurieta@hotmail.com>
2024-03-07 21:14:41 -0300

    Update on exercise in datetime modue by ariel

--- Others/Modules/Datetime/script-1-datetime.py
+++ Others/Modules/Datetime/script-1-datetime.py
@@ -1,4 +1,15 @@
 import datetime
 
+print("Date Today: ")
 date_today = datetime.date.today()
 print(date_today)
+
+
+print("\nDay Number: ")
+date_today = date_today.day
+print(date_today)
+
+
+print("\nNow: ")
+date_now = datetime.datetime.now()
+print(date_now)
